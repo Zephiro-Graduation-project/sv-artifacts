4686343f5b477d7f926ca19e03b3d296b053aa4e
Alejandro Suarez <101528810+suaracost@users.noreply.github.com>
2025-03-29 16:28:14 -0500

    Initial commit

---
+++ src/test/java/com/zephiro/artifacts/ArtifactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zephiro.artifacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
